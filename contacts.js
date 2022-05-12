381346b577400f39a94e4cbab05807c8804efa4f
desnos <benoit.desnos66@gmail.com>
2022-05-12 18:34:46 +0200

    add mails from form + empty inputs

--- contacts.js
+++ contacts.js
@@ -1,4 +1,8 @@
 const contacts = document.querySelectorAll(".contacts--animation");
+const inputs = document.querySelectorAll("input:nth-child(-n+4)");
+const select = document.querySelector("select");
+const textArea = document.querySelector("textarea");
+console.log(inputs);
 
 /* let optionsContacts = {
   root: document.querySelector("#contacts > section"),
@@ -23,61 +27,30 @@ const observerContacts = new IntersectionObserver(contactsAnim, options);
 contacts.forEach((contact) => {
   observerContacts.observe(contact);
 });
-/*  
-let pseudo = "";
-let enterprise = "";
-let Email = "";
-let Téléphone = "";
 
-const inputs = document.querySelectorAll("input");
-console.log(inputs);
-const init2 = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    pseudo: pseudo,
-    Entreprise: enterprise,
-    Email: Email,
-    Téléphone: Téléphone,
-  }),
-  mode: "cors",
-  credentials: "same-origin",
-};
-
-document.querySelector("form").addEventListener("submit", (e) => {
-  pseudo = inputs[0].value;
-  enterprise = inputs[1].value;
-  Email = inputs[2].value;
-  Téléphone = inputs[3].value;
-  
-  fetch("http://localhost:3000/posts", init2).then(() =>
-  console.log("data envoyée")
-  );
-});
-
-/* 
-document.querySelector("input[type=submit]").addEventListener("click", (e) => {
-  pseudo = inputs[0].value;
-  enterprise = inputs[1].value;
-  Email = inputs[2].value;
-  Téléphone = inputs[3].value;
-  
-  console.log(inputs[0].value);
-  console.log(inputs[1].value);
-  console.log(inputs[2].value);
-  console.log(inputs[3].value);
-  
-  fetch("http://localhost:3000/posts", init2).then(() =>
-  console.log("data envoyée")
-  );
-});
-*/
-/* 
-console.log(pseudo);
-console.log(enterprise);
-console.log(Email);
-console.log(Téléphone);
-
- */
+function sendForm() {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_mbshy4c", "contact_form", this).then(
+        function () {
+          console.log("SUCCESS!");
+          inputs.forEach((input) => {
+            input.value = "";
+          });
+          textArea.value = "";
+        },
+        function (error) {
+          console.log("FAILED...", error);
+          inputs.forEach((input) => {
+            input.value = "";
+          });
+          textArea.value = "";
+        }
+      );
+    });
+}
